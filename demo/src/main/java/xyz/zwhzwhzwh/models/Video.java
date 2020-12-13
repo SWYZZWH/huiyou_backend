c1a4efada30f1719250f5cbca6b9a6eb855cd678
ystttttt <ystttttt@users.noreply.github.com>
2020-12-13 23:06:29 +0800

    add new files for the user chart

---
+++ demo/src/main/java/xyz/zwhzwhzwh/models/Video.java
@@ -0,0 +1,4 @@
+package xyz.zwhzwhzwh.models;
+
+public class Video {
+}
---
+++ demo/src/main/java/xyz/zwhzwhzwh/repositories/UserChartRepository.java
@@ -0,0 +1,15 @@
+package xyz.zwhzwhzwh.repositories;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import org.springframework.data.repository.query.Param;
+import xyz.zwhzwhzwh.models.UserChart;
+
+// 暂时没啥函数要写的，就先放着
+public interface UserChartRepository extends MongoRepository<UserChart, String> {
+
+    UserChart findByUid(@Param("uid") String uid);
+    Boolean existsByUid(@Param("uid") String uid);
+}
